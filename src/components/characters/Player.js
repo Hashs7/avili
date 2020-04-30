c1c142772d03574d0ac8001e5f4bd60899534e2e
Sebastien Hernoux <seb_her@hotmail.fr>
2020-04-30 13:33:08 +0200

    Fix commented code

--- src/components/characters/Player.js
+++ src/components/characters/Player.js
@@ -64,7 +64,7 @@ export default class extends Character {
     });
 
     this.world.addBody(this.character.body);*/
-z
+
     const geometry = new THREE.CylinderGeometry( size.x, size.x, size.y, 8 );
     // const geometry = new THREE.BoxGeometry( mesh.size.y, mesh.size.z, mesh.size.y, 4);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true, visible: false });
@@ -82,9 +82,9 @@ z
 
   groupCamera() {
     // this.character.position.set(0, 1, 0);
-    const spotLight = new THREE.SpotLight( 0xAD9DFB, 1, 0, 0.314, 1);
-    spotLight.position.set(-12, 15, 5);
-    spotLight.lookAt(this.character.position);
+    // const spotLight = new THREE.SpotLight( 0xAD9DFB, 1, 0, 0.314, 1);
+    // spotLight.position.set(-12, 15, 5);
+    // spotLight.lookAt(this.character.position);
     // this.group.add(spotLight);
     // const spotLightHelper = new THREE.SpotLightHelper( spotLight );
     // this.group.add( spotLightHelper );
