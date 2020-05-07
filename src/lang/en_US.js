d83878aabf1f9c7c7c5363976b73d73798a123ce
Sebastien Hernoux <seb_her@hotmail.fr>
2020-05-07 02:21:13 +0200

    Copy en translation timing

--- src/lang/en_US.js
+++ src/lang/en_US.js
@@ -1,82 +1,115 @@
 export default {
-  start: {
+  black_screen: {
     0: {
-      sentence: "EN Les jeux vidéo sont une de mes passions.",
-      time: '2500',
-      last: 'false',
-    },
-    1: {
-      sentence: "EN C’est mon frère qui m’a transmis cet amour inconsidéré pour ce domaine, je le regardais jouer pendant des heures et des heures quand j’étais petite.",
-      time: '4500',
+      sentence: "EN Ce que je préfère c'est les jeux multijoueurs.",
+      time: '2000',
       last: 'true',
     },
   },
   travelling: {
     0: {
-      sentence: "EN Ce que j'apprécie le plus c’est les jeux multijoueur en équipe.",
-      time: '500',
+      sentence: "EN On peut collaborer avec des personnes du monde entier pour pouvoir arriver aux fins désirées",
+      time: '6000',
       last: 'false',
     },
     1: {
-      sentence: "EN On peut collaborer avec des personnes du monde entier pour pouvoir arriver aux fins désirées, généralement la victoire d’une partie, élaborer des stratégies...",
-      time: '1500',
+      sentence: "EN Genre généralement c'est la victoire d'une partie mais ça peut être élaborer des stratégies.",
+      time: '8000',
       last: 'false',
     },
     2: {
-      sentence: "EN C’est bien plus excitant que de jouer toute seule.",
-      time: '1500',
+      sentence: "EN C’est vraiment plus excitant que de jouer toute seule.",
+      time: '3000',
       last: 'true',
     }
   },
-  spawn: {
+  spawn_mates: {
     0: {
-      sentence: "EN Ce que j'apprécie le plus c’est les jeux multijoueur en équipe.",
-      time: '500',
+      sentence: "EN Mais voilà je suis une femme alors je n'ai pas les même expérience de jeu qu'un homme",
+      time: '5000',
       last: 'false',
     },
     1: {
-      sentence: "On peut collaborer avec des personnes du monde entier pour pouvoir arriver aux fins désirées, généralement la victoire d’une partie, élaborer des stratégies...",
-      time: '1500',
+      sentence: "EN Genre si un homme fait des erreurs on va l'insulter sur ses skills,",
+      time: '3000',
       last: 'false',
     },
     2: {
-      sentence: "C’est bien plus excitant que de jouer toute seule.",
-      time: '1500',
+      sentence: "EN Mais pour une femme c'est différent tout de suite c'est mon genre qui va porter préjudice je vais avoir le droit à des insultes sexiste, mysogynes",
+      time: '10000',
+      last: 'false',
+    },
+    3: {
+      sentence: "Du coup je parle hyper rarement dans les parties en ligne même si à certain moment ça pourrait me sorti de situation compliquée en faite",
+      time: '8000',
+      last: 'false',
+    },
+    4: {
+      sentence: "Je préfère m'abstenir parce que ma voix elle risquerai d'enclencher une vague de haine",
+      time: '8000',
       last: 'true',
     }
   },
-  projectiles: {
+  spawn_player: {
     0: {
-      sentence: "Ce que j'apprécie le plus c’est les jeux multijoueur en équipe.",
-      time: '500',
-      last: 'false',
-    },
-    1: {
-      sentence: "On peut collaborer avec des personnes du monde entier pour pouvoir arriver aux fins désirées, généralement la victoire d’une partie, élaborer des stratégies...",
-      time: '1500',
-      last: 'false',
-    },
-    2: {
-      sentence: "C’est bien plus excitant que de jouer toute seule.",
-      time: '500',
+      sentence: "Bah c'est triste à dire mais je dois me faire discrète même auprès de mes coéquipiers et puis ...",
+      time: '7000',
       last: 'true',
-    }
+    },
   },
-  insults: {
+  infiltration_introduction: {
     0: {
-      sentence: "Ce que j'apprécie le plus c’est les jeux multijoueur en équipe.",
-      time: '500',
+      sentence: "Si jamais les autres joueurs devinent que je suis une femme ça va gâcher ma partie",
+      time: '5000',
       last: 'false',
     },
     1: {
-      sentence: "On peut collaborer avec des personnes du monde entier pour pouvoir arriver aux fins désirées, généralement la victoire d’une partie, élaborer des stratégies...",
-      time: '1500',
-      last: 'false',
+      sentence: "Genre je vais devoir supporter des remarques hyper lourdes",
+      time: '3000',
+      last: 'true',
     },
-    2: {
-      sentence: "C’est bien plus excitant que de jouer toute seule.",
-      time: '500',
+  },
+  infiltration_end: {
+    0: {
+      sentence: "De toute façon même en essayant d'être discrète ils finiront toujours par découvrir que je suis une femme",
+      time: '4000',
       last: 'true',
     }
   },
+  first_badword: {
+    0: {
+      sentence: "Au début je m'efforce de pas y prêter attention",
+      time: '2000',
+      last: 'true',
+    }
+  },
+  second_badword: {
+    0: {
+      sentence: "Mais à force on craque",
+      time: '1000',
+      last: 'false',
+    },
+    1: {
+      sentence: "Ça fini par atteindre",
+      time: '1000',
+      last: 'true',
+    }
+  },
+  ending: {
+    0: {
+      sentence: "Le pire dans tout ça c'est qu'une fois la partie terminée ça continue",
+      time: '4000',
+      last: 'false',
+    },
+    1: {
+      sentence: "Genre y a des gens qui utilise mon pseudo pour me harceler sur d'autre plateforme",
+      time: '5000',
+      last: 'false',
+    },
+    2: {
+      sentence: "Et là il est plus question de jeu",
+      time: '1000',
+      last: 'true',
+    },
+  }
 };
